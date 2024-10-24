3f3740b656247fafb2a121b1b12ee2789f251b8d
eyazici90 <e.yazici1990@gmail.com>
2024-10-24 20:35:58 +0200

    chore: make csv writing context aware
    Check context during looping on rules.
    Extract func. to improve readability.

--- internal/export.go
+++ internal/export.go
@@ -47,39 +47,56 @@ func (re *RuleExporter) Export(ctx context.Context) error {
 	const batchSize, numCol = 100, 5
 	w := csv.NewWriter(f)
 	buf := make([]string, numCol)
-	buf[0], buf[1], buf[2], buf[3], buf[4] = "type", "name", "query", "evalTime", "lastEval"
-	if err = w.Write(buf); err != nil {
+	if err = re.writeHeaders(w, buf); err != nil {
 		return fmt.Errorf("write headers: %w", err)
 	}
 
 	var n uint16
 	for _, group := range rules.Groups {
 		for _, rule := range group.Rules {
-			if n >= batchSize {
-				w.Flush()
-				if err = w.Error(); err != nil {
-					return fmt.Errorf("flush csv: %w", err)
-				}
-				n = 0
-			}
-			n++
-			switch v := rule.(type) {
-			case promapiv1.RecordingRule:
-				buf[0], buf[1], buf[2] = "record", v.Name, v.Query
-				buf[3], buf[4] = strconv.FormatFloat(v.EvaluationTime, 'g', -1, 64), v.LastEvaluation.String()
-				if err = w.Write(buf); err != nil {
-					return fmt.Errorf("write recording rule: %w", err)
-				}
-			case promapiv1.AlertingRule:
-				buf[0], buf[1], buf[2] = "alert", v.Name, v.Query
-				buf[3], buf[4] = strconv.FormatFloat(v.EvaluationTime, 'g', -1, 64), v.LastEvaluation.String()
-				if err = w.Write(buf); err != nil {
-					return fmt.Errorf("write alerting rule: %w", err)
-				}
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
 			default:
+				if n >= batchSize {
+					w.Flush()
+					if err = w.Error(); err != nil {
+						return fmt.Errorf("flush csv: %w", err)
+					}
+					n = 0
+				}
+				n++
+				switch r := rule.(type) {
+				case promapiv1.RecordingRule:
+					if err = re.writeRecordingRule(w, buf, r); err != nil {
+						return fmt.Errorf("write recording rule: %w", err)
+					}
+				case promapiv1.AlertingRule:
+					if err = re.writeAlertingRule(w, buf, r); err != nil {
+						return fmt.Errorf("write alerting rule: %w", err)
+					}
+				default:
+				}
 			}
 		}
 	}
 	w.Flush()
 	return nil
+}
+
+func (re *RuleExporter) writeHeaders(w *csv.Writer, buf []string) error {
+	buf[0], buf[1], buf[2], buf[3], buf[4] = "type", "name", "query", "evalTime", "lastEval"
+	return w.Write(buf)
+}
+
+func (re *RuleExporter) writeRecordingRule(w *csv.Writer, buf []string, r promapiv1.RecordingRule) error {
+	buf[0], buf[1], buf[2] = "record", r.Name, r.Query
+	buf[3], buf[4] = strconv.FormatFloat(r.EvaluationTime, 'g', -1, 64), r.LastEvaluation.String()
+	return w.Write(buf)
+}
+
+func (re *RuleExporter) writeAlertingRule(w *csv.Writer, buf []string, r promapiv1.AlertingRule) error {
+	buf[0], buf[1], buf[2] = "alert", r.Name, r.Query
+	buf[3], buf[4] = strconv.FormatFloat(r.EvaluationTime, 'g', -1, 64), r.LastEvaluation.String()
+	return w.Write(buf)
 }
